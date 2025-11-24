1bda79f08c5388e75a1e321e4deb59abbc45a617
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-11-24 12:13:03 -0300

    Add Pessoa Controller and Exception

--- src/main/java/com/barbearia/EPBD/service/PessoaService.java
+++ src/main/java/com/barbearia/EPBD/service/PessoaService.java
@@ -1,13 +1,13 @@
 package com.barbearia.EPBD.service;
 
 import com.barbearia.EPBD.dto.pessoaDTO.PessoaResponseDTO;
-import com.barbearia.EPBD.model.Pessoa;
+import com.barbearia.EPBD.exception.ResourceNotFoundException;
 import com.barbearia.EPBD.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NullMarked;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,21 +15,23 @@ public class PessoaService {
 
     private final PessoaRepository pessoaRepository;
 
-    @NullMarked
+    @Transactional(readOnly = true)
     public Page<PessoaResponseDTO> findAll(Pageable pageable) {
         return pessoaRepository.findAll(pageable)
                 .map(PessoaResponseDTO::new);
     }
 
+    @Transactional(readOnly = true)
     public PessoaResponseDTO findByCpf(String cpf) {
-        return pessoaRepository.findById(cpf).
-                map(PessoaResponseDTO::new).
-                orElse(null);
+        return pessoaRepository.findById(cpf)
+                .map(PessoaResponseDTO::new)
+                .orElseThrow(() -> new ResourceNotFoundException("Pessoa não encontrada com CPF: " + cpf));
     }
 
+    @Transactional(readOnly = true)
     public PessoaResponseDTO findByEmail(String email) {
-        return pessoaRepository.findByEmail(email).
-                map(PessoaResponseDTO::new).
-                orElse(null);
+        return pessoaRepository.findByEmail(email)
+                .map(PessoaResponseDTO::new)
+                .orElseThrow(() -> new ResourceNotFoundException("Email não encontrado: " + email));
     }
 }
