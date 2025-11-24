8741ff89c8f640dd5dafb5eb4146a7c84f62a140
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-11-24 09:55:59 -0300

    first commit

---
+++ src/main/java/com/barbearia/EPBD/EpbdApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.EPBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbearia/EPBD/EpbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.EPBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
