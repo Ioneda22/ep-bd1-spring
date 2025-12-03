e8c21d35d2cb83375cd4aaf8f31aa90ba0c9d3d6
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-12-03 10:50:33 -0300

    Add Cliente Mapper

---
+++ src/main/java/com/barbearia/EPBD/mapper/ClienteMapper.java
@@ -0,0 +1,15 @@
+package com.barbearia.EPBD.mapper;
+
+import com.barbearia.EPBD.dto.clienteDTO.ClienteRequestDTO;
+import com.barbearia.EPBD.dto.clienteDTO.ClienteResponseDTO;
+import com.barbearia.EPBD.model.Cliente;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ClienteMapper {
+    ClienteResponseDTO toResponseDTO(Cliente cliente);
+
+    @Mapping(target="senha", ignore=true)
+    Cliente toEntity(ClienteRequestDTO dto);
+}
