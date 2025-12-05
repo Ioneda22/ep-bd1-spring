7ff2fdbdac91d1c992f9d9c3795644e3aff28031
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-12-05 11:07:10 -0300

    Adjust Id Path Variable in Servico Controller

--- src/main/java/com/barbearia/EPBD/controller/ServicoController.java
+++ src/main/java/com/barbearia/EPBD/controller/ServicoController.java
@@ -2,12 +2,14 @@ package com.barbearia.EPBD.controller;
 
 import com.barbearia.EPBD.dto.servicoDTO.ServicoResponseDTO;
 import com.barbearia.EPBD.service.ServicoService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +22,15 @@ public class ServicoController {
 
     @GetMapping
     public ResponseEntity<Page<ServicoResponseDTO>> findAll(
-            @PageableDefault(size = 10, sort = "id") Pageable pageable) {
+            @PageableDefault(size = 10, sort = "Id") Pageable pageable) {
         return ResponseEntity.ok(servicoService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ServicoResponseDTO> findById(Integer id) {
+    public ResponseEntity<ServicoResponseDTO> findById(
+            @PathVariable
+            @Min(value = 1, message = "Id deve ser maior que 0")
+            Integer id) {
         return ResponseEntity.ok(servicoService.findById(id));
     }
 }
