e16197e97a33496b0107bfb4eafff5243a297af4
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-12-05 14:03:49 -0300

    Add Servico update and delete in ServicoService

--- src/main/java/com/barbearia/EPBD/service/ServicoService.java
+++ src/main/java/com/barbearia/EPBD/service/ServicoService.java
@@ -1,8 +1,13 @@
 package com.barbearia.EPBD.service;
 
+import com.barbearia.EPBD.dto.servicoDTO.ServicoRequestDTO;
 import com.barbearia.EPBD.dto.servicoDTO.ServicoResponseDTO;
+import com.barbearia.EPBD.exception.BusinessRuleException;
 import com.barbearia.EPBD.exception.ResourceNotFoundException;
 import com.barbearia.EPBD.mapper.ServicoMapper;
+import com.barbearia.EPBD.model.Barbeiro;
+import com.barbearia.EPBD.model.Servico;
+import com.barbearia.EPBD.repository.BarbeiroRepository;
 import com.barbearia.EPBD.repository.ServicoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,11 +15,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ServicoService {
 
     private final ServicoRepository servicoRepository;
+    private final BarbeiroRepository barbeiroRepository;
     private final ServicoMapper servicoMapper;
 
     @Transactional(readOnly = true)
@@ -28,5 +36,37 @@ public class ServicoService {
         return servicoRepository.findById(id)
                 .map(servicoMapper::toResponseDto)
                 .orElseThrow(() -> new ResourceNotFoundException("Servico com id " + id + " não encontrado."));
+    }
+
+    @Transactional
+    public ServicoResponseDTO create(ServicoRequestDTO servicoRequestDTO) {
+        Servico servico = servicoMapper.toEntity(servicoRequestDTO);
+        servicoRepository.save(servico);
+
+        return servicoMapper.toResponseDto(servico);
+    }
+
+    @Transactional
+    public ServicoResponseDTO update(ServicoRequestDTO servicoRequestDTO, Integer id) {
+        Servico servico = servicoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Servico com id " + id + " não encontrado."));
+
+        servico.setNome(servicoRequestDTO.getNome());
+        servico.setDescricao(servicoRequestDTO.getDescricao());
+        servico.setPreco(servicoRequestDTO.getPreco());
+        servico.setDuracaoEstimadaMin(servicoRequestDTO.getDuracaoEstimadaMin());
+
+        servicoRepository.save(servico);
+
+        return servicoMapper.toResponseDto(servico);
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        if (!servicoRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Servico com id " + id + " não encontrado.");
+        }
+
+        servicoRepository.deleteById(id);
     }
 }
