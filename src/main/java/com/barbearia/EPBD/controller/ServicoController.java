e162b194ba1c2ed71bb34d9b3a233b052abd84e7
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-12-05 14:22:06 -0300

    Add Servico Patch and Delete in Service

--- src/main/java/com/barbearia/EPBD/controller/ServicoController.java
+++ src/main/java/com/barbearia/EPBD/controller/ServicoController.java
@@ -1,7 +1,9 @@
 package com.barbearia.EPBD.controller;
 
 import com.barbearia.EPBD.dto.servicoDTO.ServicoResponseDTO;
+import com.barbearia.EPBD.dto.servicoDTO.ServicoUpdateDTO;
 import com.barbearia.EPBD.service.ServicoService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,10 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("barbearia/v1/servicos")
@@ -34,5 +33,23 @@ public class ServicoController {
             @Min(value = 1, message = "Id deve ser maior que 0")
             Integer id) {
         return ResponseEntity.ok(servicoService.findById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ServicoResponseDTO> updateEntity(
+            @RequestBody @Valid ServicoUpdateDTO dto,
+            @PathVariable
+            @Min(value = 1, message = "Id deve ser maior que 0")
+            Integer id) {
+        return ResponseEntity.ok(servicoService.update(dto, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEntity(
+            @PathVariable
+            @Min(value = 1, message = "Id deve ser maior que 0")
+            Integer id) {
+        servicoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
