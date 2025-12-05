d442e0c2b2a834465cbd705bc31c8bbce1cbee63
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-12-05 10:25:03 -0300

    Add Servico base Repository and Service

---
+++ src/main/java/com/barbearia/EPBD/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.barbearia.EPBD.repository;
+
+import com.barbearia.EPBD.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
