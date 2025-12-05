dece2cb71cbc23f2659067cc041d565b92d1aea3
Bruno Ioneda <brunohideoioneda@gmail.com>
2025-12-05 10:52:04 -0300

    Adjust Servico and Barbeiro ResponseDTOs

--- src/main/java/com/barbearia/EPBD/dto/barbeiroDTO/BarbeiroResponseDTO.java
+++ src/main/java/com/barbearia/EPBD/dto/barbeiroDTO/BarbeiroResponseDTO.java
@@ -1,13 +1,17 @@
 package com.barbearia.EPBD.dto.barbeiroDTO;
 
 import com.barbearia.EPBD.dto.pessoaDTO.PessoaResponseDTO;
+import com.barbearia.EPBD.dto.servicoDTO.ServicoResumoDTO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class BarbeiroResponseDTO extends PessoaResponseDTO {
     private LocalDate dataInicio;
+
+    private List<ServicoResumoDTO> servicos;
 }
--- src/main/java/com/barbearia/EPBD/dto/servicoDTO/ServicoResponseDTO.java
+++ src/main/java/com/barbearia/EPBD/dto/servicoDTO/ServicoResponseDTO.java
@@ -1,8 +1,10 @@
 package com.barbearia.EPBD.dto.servicoDTO;
 
+import com.barbearia.EPBD.dto.barbeiroDTO.BarbeiroResumoDTO;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class ServicoResponseDTO {
@@ -11,4 +13,5 @@ public class ServicoResponseDTO {
     private BigDecimal preco;
     private Integer duracaoEstimadaMin;
     private String descricao;
+    private List<BarbeiroResumoDTO> barbeiros;
 }
